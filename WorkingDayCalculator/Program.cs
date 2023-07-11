c48522834ec9ee27fa1282f75475832bec0af80f
leega <leega@LAPTOP-3JC41ES6>
2023-07-11 21:42:35 +0100

    Support for working week

--- WorkingDayCalculator/Program.cs
+++ WorkingDayCalculator/Program.cs
@@ -11,12 +11,33 @@ public class Program
     public static void Main(string[] args)
     {
         var (serviceProvider, confiuration) = Configure(args);
-        var currentDate = DateTime.Now;
+        var currentDate = DateTime.Now.AddDays(-3);
         var dayCalculatorService = serviceProvider.GetRequiredService<IDayCalculatorService>();
-        var workingTime = dayCalculatorService.GetWorkingTime(currentDate);
 
-        Console.WriteLine($"{currentDate.ToString("dd-MM-yyyy")} - {workingTime.WorkingHours.ToHoursAndMinutesString()} - (Start: {workingTime.StartTime.ToHoursAndMinutesString()}, End: {workingTime.EndTime.ToHoursAndMinutesString()})");
+        if (args.Any() && args[0].ToLower() == "week")
+        {
+            var dates = GetWorkingDates(currentDate);
+            foreach (var date in dates)
+            {
+                var workingTime = dayCalculatorService.GetWorkingTime(date);
+                Console.WriteLine($"{date.ToString("dd-MM-yyyy")} - {workingTime.WorkingHours.ToHoursAndMinutesString()} - (Start: {workingTime.StartTime.ToHoursAndMinutesString()}, End: {workingTime.EndTime.ToHoursAndMinutesString()})");
+            }
+        }
+
+        var startWorkingDay = dayCalculatorService.GetWorkingTime(currentDate);
+        Console.WriteLine($"{currentDate.ToString("dd-MM-yyyy")} - {startWorkingDay.WorkingHours.ToHoursAndMinutesString()} - (Start: {startWorkingDay.StartTime.ToHoursAndMinutesString()}, End: {startWorkingDay.EndTime.ToHoursAndMinutesString()})");
+
         Console.ReadLine();
+    }
+
+    private static List<DateTime> GetWorkingDates(DateTime startDate)
+    {
+        var workingDates = new List<DateTime>();
+        var totalDates = startDate.DayOfWeek - DayOfWeek.Monday;
+        for (var i = totalDates; i > 0; i--)
+            workingDates.Add(startDate.AddDays(-i));
+
+        return workingDates;
     }
 
     private static (IServiceProvider, IConfiguration) Configure(string[] args)
